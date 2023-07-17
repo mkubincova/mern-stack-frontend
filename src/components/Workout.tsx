35869e2375e5aed491047c4703642b0e13097ae0
mkubincova <mkubincova@proton.me>
2023-07-17 13:23:21 +0200

    feat: delete data

--- src/components/Workout.tsx
+++ src/components/Workout.tsx
@@ -1,16 +1,27 @@
 import { IWorkout } from '../vite-env';
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
 
 type Props = {
     workout: IWorkout;
 };
 
 export default function Workout({ workout }: Props) {
+    const { dispatch } = useWorkoutsContext();
+    const handleClick = async () => {
+        const res = await fetch(`${import.meta.env.VITE_API_URL}/workouts/${workout._id}`, {
+            method: 'DELETE'
+        });
+        const json = await res.json();
+
+        if (res.ok) dispatch({ type: 'DELETE_WORKOUT', payload: json });
+    };
     return (
         <div className="workout-details">
             <h4>{workout.title}</h4>
             <p><strong>Load (kg):</strong> {workout.load.toString()}</p>
             <p><strong>Reps:</strong> {workout.reps.toString()}</p>
             <p>{workout.createdAt}</p>
+            <span onClick={handleClick}>delete</span>
         </div>
     );
 }
--- src/context/WorkoutContext.tsx
+++ src/context/WorkoutContext.tsx
@@ -22,6 +22,10 @@ export const workoutsReducer = (state: InitialStateType, action: IAction) => {
             return {
                 workouts: [action.payload, ...state.workouts]
             };
+        case 'DELETE_WORKOUT':
+            return {
+                workouts: state.workouts.filter((workout) => workout._id !== action.payload._id)
+            };
         default:
             return state;
     }
