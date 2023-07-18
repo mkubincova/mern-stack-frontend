14f1d4b757f9cd4cac7df0e7be014f464ab1bff1
mkubincova <mkubincova@proton.me>
2023-07-18 09:51:44 +0200

    fix: folder name

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { WorkoutContextProvider } from "./context/WorkoutContext.tsx";
+import { WorkoutContextProvider } from "./contexts/WorkoutContext.tsx";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
